5bd728525237689f6befcb8305eec456888fe27d
alesyacurtis <154752137+alesyacurtis@users.noreply.github.com>
2025-06-18 19:30:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
+#include <fstream>
 #include "Solve.h"
 
 using namespace std;
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	Solver s;
+	SolverModif sm;
+	SolverNew sn;
+	SolverRoot sr;
+	File f;
 
-//	s.SetParams();
+	Practice p;
 
-	s.Solve();
-	//Trial t = s.GetSolution();
+	int n = 1000;
+	vector <int> K(n);
+
+	p.BA(K,n);
+	//f.InputToFile_percent(K, n, 1);
+
+	p.BA_Modif(K, n);
+	//f.InputToFile_percent(K, n, 2);
+
+	//p.NewA(K, n);
+	//f.InputToFile_percent(K, n, 3);
+
+	//p.RootA(K,n);
+	//f.InputToFile_percent(K, n, 4);
 }
